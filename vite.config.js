998128dc6add5fbfb7aa0bce7bccc27a499736a4
Your Name <Nadunkeys@gmail.com>
2026-02-21 15:50:35 +0530

    to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react' // 1. Add this import
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/DHAARA-WEB/',
   plugins: [
     react(), // 2. Add this to the plugins array
     tailwindcss(),
