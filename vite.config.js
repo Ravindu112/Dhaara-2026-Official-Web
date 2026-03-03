bb77d9dad617d04cc22a74990d8e7c95ec8f0ad0
Ravindu112 <141537229+Ravindu112@users.noreply.github.com>
2026-03-03 10:28:19 +0530

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react' // 1. Add this import
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/DHAARA-WEB/',
+  base: '/',
   plugins: [
     react(), // 2. Add this to the plugins array
     tailwindcss(),
